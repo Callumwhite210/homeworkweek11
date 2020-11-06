7774af7a774720875cb5a328ef4c0524bdfc2fa5
Callum White <callumwhite210@gmail.com>
2020-11-06 14:00:21 +1030

    progress

--- Develop/app.js
+++ Develop/app.js
@@ -1,4 +1,4 @@
-//Declared Variables 
+//For Node 
 const express = require("express");
 const fs = require("fs");
 const path = require("path")
@@ -7,8 +7,14 @@ const app = express();
 //Server Port
 let PORT = process.env.PORT || 3000;
 
+//Show that app is listening and running
+app.listen(PORT, function(){
+    console.log('App listening:'+ PORT)
+});
+
 //Express
 app.use(express.urlencoded({extended: true}));
+app.use(express.static("public"));
 app.use(express.json());
 
 //Linking assets
@@ -18,6 +24,28 @@ app.use('./assets/css',express.static(path.join(__dirname,'public/assets/css')))
 //Array for user notes
 let userNotes = [];
 
-app.listen(PORT, function(){
-    console.log('App listening:'+ PORT)
+//Grabs respective HTMLs
+app.get("./index",function(req,res){
+    res.sendFile(path.join(__dirname, "./public/index.html"));
 });
+
+app.get("./notes",function(req,res){
+    res.sendFile(path.join(__dirname, "./public/notes.html"));
+});
+
+//Reads and returns data in notes Json
+const noteJson = fs.readFile(path.join(__dirname, "/db/db.json"), function(err, note){
+    if (note){
+        console.log("Notes: "+ note);
+        return note;
+    }
+    if (err){
+        console.error(err);
+    }
+});
+
+app.get("./app/notes", function(req, res){
+    res.json(noteJson);
+});
+
+
