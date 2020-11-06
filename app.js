40a437d56fa3905a022b1687734dbce7f0591751
Callum White <callumwhite210@gmail.com>
2020-11-06 13:19:36 +1030

    set up

---
+++ app.js
@@ -0,0 +1,21 @@
+//Declared Variables 
+const express = require("express");
+const fs = require("fs");
+const path = require("path")
+const app = express();
+
+//Server Port
+let PORT = process.env.PORT || 3000;
+
+//Express
+app.use(express.urlencoded({extended: true}));
+app.use(express.json());
+
+//Linking assets
+app.use('./assets/js',express.static(path.join(__dirname,'public/assets/js')));
+app.use('./assets/css',express.static(path.join(__dirname,'public/assets/css')));
+
+//Array for user notes
+let userNotes = [];
+
+
